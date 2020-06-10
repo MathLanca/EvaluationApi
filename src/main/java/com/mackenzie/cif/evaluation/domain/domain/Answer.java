91b9fcfc2dcc364099a17757749d8a7102e94f5f
mathLanca <matheus.7910@hotmail.com>
2020-06-10 19:47:06 -0300

    Alterando variavel de cGrade e pGrade

--- src/main/java/com/mackenzie/cif/evaluation/domain/domain/Answer.java
+++ src/main/java/com/mackenzie/cif/evaluation/domain/domain/Answer.java
@@ -1,19 +1,17 @@
 package com.mackenzie.cif.evaluation.domain.domain;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Answer {
     private String questionId;
     private String infoSource;
     private String problemDescription;
     private BigDecimal generalGrade;
-    private BigDecimal cGrade;
-    private BigDecimal pGrade;
+    private BigDecimal capacityGrade;
+    private BigDecimal performanceGrade;
     private BigDecimal locationGrade;
     private BigDecimal extensionGrade;
     private BigDecimal natureGrade;
